f56dd60665ffb02bf6b73499525f7ef9d810b706
Nicollas Willian Amaral da Silva <67935302+NikcWill@users.noreply.github.com>
2023-02-24 21:37:35 -0300

    api ibge

--- Exercico/api.dart
+++ Exercico/api.dart
@@ -5,8 +5,18 @@ void main(List<String> args) {
   getAPI();
 
 }
+
+
 getAPI () async {
-  Uri url = Uri.parse("https://jsonplaceholder.typicode.com/todos/1");
+  Uri url = Uri.parse("https://servicodados.ibge.gov.br/api/v2/censos/nomes/nicollas");
   var response = await http.get(url);
-  print(jsonDecode(response.body)['title']);
+  print(jsonDecode(response.body)[0]['nome']);
+
+for (int i = 0; i < (jsonDecode(response.body)[0]['res']).length; i++){
+  print('O periodo é ${jsonDecode(response.body)[0]['res'][i]['periodo']}');
+  print('A Frequencia é ${jsonDecode(response.body)[0]['res'][i]['frequencia']}');
+  print('___________________');
+}  
+  
+
 }
