a02ff6b09597c770f3e5731ac74b78659adea6f7
Nicollas Willian Amaral da Silva <67935302+NikcWill@users.noreply.github.com>
2023-02-26 21:56:42 -0300

    alterações feitas em casa

--- Exercico/api.dart
+++ Exercico/api.dart
@@ -3,20 +3,18 @@ import 'dart:convert';
 
 void main(List<String> args) {
   getAPI();
-
 }
 
-
-getAPI () async {
-  Uri url = Uri.parse("https://servicodados.ibge.gov.br/api/v2/censos/nomes/nicollas");
+getAPI() async {
+  Uri url = Uri.parse(
+      "https://servicodados.ibge.gov.br/api/v2/censos/nomes/nicollas");
   var response = await http.get(url);
   print(jsonDecode(response.body)[0]['nome']);
 
-for (int i = 0; i < (jsonDecode(response.body)[0]['res']).length; i++){
-  print('O periodo é ${jsonDecode(response.body)[0]['res'][i]['periodo']}');
-  print('A Frequencia é ${jsonDecode(response.body)[0]['res'][i]['frequencia']}');
-  print('___________________');
-}  
-  
-
+  for (int i = 0; i < (jsonDecode(response.body)[0]['res']).length; i++) {
+    print('O periodo é ${jsonDecode(response.body)[0]['res'][i]['periodo']}');
+    print(
+        'A Frequencia é ${jsonDecode(response.body)[0]['res'][i]['frequencia']}');
+    print('___________________');
+  }
 }
