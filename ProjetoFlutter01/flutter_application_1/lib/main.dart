aca1feff6226360077adbb0277aa666b2bd588ea
Nicollas Willian Amaral da Silva <67935302+NikcWill@users.noreply.github.com>
2023-03-03 20:04:05 -0300

    Atualiza

--- ProjetoFlutter01/flutter_application_1/lib/main.dart
+++ ProjetoFlutter01/flutter_application_1/lib/main.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
 
-void void main(List<String> args) {
-    runApp(MaterialApp(
+void main(List<String> args) {
+  runApp(
+    MaterialApp(
       home: HomePage(),
-
+      debugShowCheckedModeBanner: false,
     ),
   );
 }
-stl
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'NuBank',
+          ),
+          backgroundColor: Colors.purple,
+          centerTitle: true,
+        ),
+        body: Row(
+          children: [
+            Container(
+              color: Colors.grey.shade600,
+              width: 200,
+              height: 200,
+              child: Center(
+                child: Text('Teu saldo é de 1.000.000'),
+              ),
+            ),
+            Container(
+              color: Colors.grey.shade600,
+              width: 200,
+              height: 200,
+              child: Center(
+                child: Text('Ta rico'),
+              ),
+            ),
+          ],
+        ));
+  }
+}
