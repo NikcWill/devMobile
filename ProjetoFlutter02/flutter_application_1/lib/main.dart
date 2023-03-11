1d65695de2ea5806f32dc39d0e2f63d1ed793f1b
Nicollas Willian Amaral da Silva <67935302+NikcWill@users.noreply.github.com>
2023-03-10 21:34:05 -0300

    Exercicio de sala

--- ProjetoFlutter02/flutter_application_1/lib/main.dart
+++ ProjetoFlutter02/flutter_application_1/lib/main.dart
@@ -5,17 +5,80 @@ void main() {
     MaterialApp(
       home: HomePage(),
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.green),
     ),
   );
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  PageController _pagecontroller = PageController();
+
+  int active = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(),
+      backgroundColor: Colors.grey.shade300,
+      drawer: Drawer(
+        child: ListView(children: [
+          UserAccountsDrawerHeader(
+            accountName: Text('Nicollas'),
+            accountEmail: Text('nikcwill@gmail.com'),
+            currentAccountPicture: CircleAvatar(
+              child: Text('N'),
+            ),
+          ),
+          ListTile(
+            onTap: () {
+              _pagecontroller.jumpToPage(1);
+              Navigator.pop(context);
+              setState(() {
+                active = 1;
+              });
+            },
+            onLongPress: () => print('Estou na home'),
+            title: Text('pagina 01'),
+            trailing: Icon(Icons.category),
+            leading: CircleAvatar(),
+            subtitle: Text('Toma'),
+          ),
+          ListTile(
+            onTap: () {
+              _pagecontroller.jumpToPage(2);
+              Navigator.pop(context);
+              setState(() {
+                active = 2;
+              });
+            },
+            onLongPress: () => print('Estou na home'),
+            title: Text('Home'),
+            trailing: Icon(Icons.home),
+            leading: CircleAvatar(),
+            subtitle: Text('Toma'),
+          ),
+          ListTile(
+            onTap: () {
+              _pagecontroller.jumpToPage(3);
+              Navigator.pop(context);
+              setState(() {
+                active = 3;
+              });
+            },
+            onLongPress: () => print('Estou na home'),
+            title: Text('Home'),
+            trailing: Icon(Icons.home),
+            leading: CircleAvatar(),
+            subtitle: Text('Toma'),
+          ),
+        ]),
+      ),
       appBar: AppBar(
         actions: [
           IconButton(
@@ -49,85 +112,109 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.blue.shade600,
         centerTitle: false,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: PageView(
+        controller: _pagecontroller,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Container(
-                color: Colors.grey.shade600,
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height / 3,
-                child: Center(
-                  child: Text('Teu saldo é de 1.000.000'),
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      color: Colors.grey.shade600,
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height / 3,
+                      child: Center(
+                        child: Text('Teu saldo é de 1.000.000'),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      color: Colors.grey.shade600,
+                      width: 50,
+                      height: 50,
+                      child: Center(
+                        child: Text('Teu saldo é de 1.000.000'),
+                      ),
+                    ),
+                    Container(
+                      color: Colors.grey.shade600,
+                      width: 50,
+                      height: 50,
+                      child: Center(
+                        child: Text('Ta rico'),
+                      ),
+                    ),
+                    Container(
+                      color: Colors.grey.shade600,
+                      width: 50,
+                      height: 50,
+                      child: Center(
+                        child: Text('Ta rico'),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      color: Colors.grey.shade600,
+                      width: 50,
+                      height: 50,
+                      child: Center(
+                        child: Text('Teu saldo é de 1.000.000'),
+                      ),
+                    ),
+                    Container(
+                      color: Colors.grey.shade600,
+                      width: 50,
+                      height: 50,
+                      child: Center(
+                        child: Text('Ta rico'),
+                      ),
+                    ),
+                    Container(
+                      color: Colors.grey.shade600,
+                      width: 50,
+                      height: 50,
+                      child: Center(
+                        child: Text('Ta rico'),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Container(
-                color: Colors.grey.shade600,
-                width: 50,
-                height: 50,
-                child: Center(
-                  child: Text('Teu saldo é de 1.000.000'),
-                ),
-              ),
-              Container(
-                color: Colors.grey.shade600,
-                width: 50,
-                height: 50,
-                child: Center(
-                  child: Text('Ta rico'),
-                ),
-              ),
-              Container(
-                color: Colors.grey.shade600,
-                width: 50,
-                height: 50,
-                child: Center(
-                  child: Text('Ta rico'),
-                ),
-              ),
-            ],
+          Container(
+            color: Colors.amber,
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Container(
-                color: Colors.grey.shade600,
-                width: 50,
-                height: 50,
-                child: Center(
-                  child: Text('Teu saldo é de 1.000.000'),
-                ),
-              ),
-              Container(
-                color: Colors.grey.shade600,
-                width: 50,
-                height: 50,
-                child: Center(
-                  child: Text('Ta rico'),
-                ),
-              ),
-              Container(
-                color: Colors.grey.shade600,
-                width: 50,
-                height: 50,
-                child: Center(
-                  child: Text('Ta rico'),
-                ),
-              ),
-            ],
-          )
+          Container(
+            color: Colors.pink,
+          ),
+          Container(
+            color: Colors.red,
+          ),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
         backgroundColor: Colors.blue.shade600,
+        type: BottomNavigationBarType.fixed,
+        currentIndex: active,
+        onTap: (value) {
+          _pagecontroller.animateToPage(value,
+              duration: Duration(milliseconds: 300), curve: Curves.ease);
+          setState(() {
+            active = value;
+          });
+        },
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
