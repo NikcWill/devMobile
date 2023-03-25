eee4faeeac8326ff339a51732838d86f45c234f6
Nicollas Willian Amaral da Silva <67935302+NikcWill@users.noreply.github.com>
2023-03-24 21:58:08 -0300

    Projeto do JOKEMPO

---
+++ projetoFlutter06/flutter_application_1/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Jo-Ken -Po'),
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        color: Colors.amber,
+        child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          Text('Sua Jogada'),
+          CircleAvatar(radius: 60),
+          CircleAvatar(radius: 60),
+          CircleAvatar(radius: 60),
+        ]),
+      ),
+    );
+  }
+}
