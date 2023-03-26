39f445d86909d0996278a4293637a73bcf99bbd6
Nicollas Willian Amaral da Silva <67935302+NikcWill@users.noreply.github.com>
2023-03-26 17:21:53 -0300

    Funcinando o jogo de pedra papel e tesoura

--- projetoFlutter06/flutter_application_1/lib/main.dart
+++ projetoFlutter06/flutter_application_1/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,6 +17,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List jogo = ['Pedra', 'Papel', 'Tesoura'];
+
+  void gerarJogadaPc() {
+    var gerarNumero = Random().nextInt(jogo.length);
+    var jogadaSorteada = jogo[gerarNumero];
+    setState(() {
+      jogadaPc = jogadaSorteada;
+    });
+
+    if (jogadaVoce == jogadaSorteada) {
+      setState(() {
+        resultado = 'Empate! Jogue novamente!';
+      });
+    } else if (jogadaVoce == 'Pedra' && jogadaPc == 'Tesoura' ||
+        jogadaVoce == 'Papel' && jogadaPc == 'Pedra' ||
+        jogadaVoce == 'Tesoura' && jogadaPc == 'Papel') {
+      setState(() {
+        resultado = 'Você Ganhou';
+        contVc = contVc + 1;
+      });
+    } else {
+      setState(() {
+        resultado = 'O PC Ganhou';
+        contPc = contPc + 1;
+      });
+    }
+  }
+
+  var jogadaPc = 'PC Aguardando!';
+  var jogadaVoce = '';
+  var resultado = 'Aguardadno';
+  var contVc = 0;
+  var contPc = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,19 +58,136 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         title: Text('Jo-Ken -Po'),
       ),
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        color: Colors.amber,
-        child: Column(
-          children: [
-            Text('Sua Jogada'),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-              CircleAvatar(radius: 60),
-              CircleAvatar(radius: 60),
-              CircleAvatar(radius: 60),
-            ]),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(50),
+                  child: Text('Sua Jogada'),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        jogadaVoce = jogo[0];
+                        gerarJogadaPc();
+                      },
+                      child: CircleAvatar(
+                        child: Text(jogo[0]),
+                        radius: 80,
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        jogadaVoce = jogo[1];
+                        gerarJogadaPc();
+                      },
+                      child: CircleAvatar(
+                        child: Text(jogo[1]),
+                        radius: 80,
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        jogadaVoce = jogo[2];
+                        gerarJogadaPc();
+                      },
+                      child: CircleAvatar(
+                        child: Text(jogo[2]),
+                        radius: 80,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  child: Text('Jogada do computador'),
+                  padding: EdgeInsets.all(50),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    CircleAvatar(
+                      child: Text(jogadaPc),
+                      radius: 80,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  child: Text('resultado'),
+                  padding: EdgeInsets.all(50),
+                ),
+                Container(
+                  child: Text(resultado),
+                  padding: EdgeInsets.all(5),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  child: Text('Placar'),
+                  padding: EdgeInsets.all(20),
+                ),
+                Container(
+                  width: 400,
+                  height: 200,
+                  decoration: BoxDecoration(
+                      color: Colors.grey[400],
+                      borderRadius: BorderRadius.circular(70)),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text('Voce'),
+                            Text('PC'),
+                          ],
+                        ),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text(
+                                contVc.toString(),
+                                style: TextStyle(
+                                  fontSize: 60,
+                                ),
+                              ),
+                              Text(
+                                contPc.toString(),
+                                style: TextStyle(
+                                  fontSize: 60,
+                                ),
+                              ),
+                            ])
+                      ]),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
