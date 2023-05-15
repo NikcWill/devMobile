3b6d3da6218c378e1020e92fb86da526d0135edc
Nicollas Willian Amaral da Silva <67935302+NikcWill@users.noreply.github.com>
2023-05-14 23:49:45 -0300

    tentando tratar o json ainda sem sucesso

--- ProjetoFluter09/atm_consultoria/lib/clientePAge.dart
+++ ProjetoFluter09/atm_consultoria/lib/clientePAge.dart
@@ -1,21 +1,37 @@
+import 'package:atm_consultoria/empresaPage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:atm_consultoria/ClientesBD.dart';
+
 
 class ClientePage extends StatefulWidget {
-  const ClientePage({super.key});
+
+ ClientePage({super.key});
 
   @override
   State<ClientePage> createState() => _ClientePageState();
 }
 
 class _ClientePageState extends State<ClientePage> {
+
+var clineteDB = ClienteDB.clientes[0]['users'];
+
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Clientes'),
       ),
+      body: Container(
+          child: ElevatedButton(
+                        onPressed: () {print(clineteDB);
+                          },child: Text("clicar"),
+
+       
+
+      ),)
     );
   }
 }
